69ca6d9e4edf5b4b51cbe9679e805b858e7033d0
Gustavo <84883113+gtacouto@users.noreply.github.com>
2022-08-27 01:05:28 +0000

    Aula 26/08

--- Graph.java
+++ Graph.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Graph {
 
   private int countNodes;
@@ -12,20 +14,36 @@ public class Graph {
 
   public void addEdge(int source, int sink, int weight) {
     if (source < 0 || source > this.adjMatrix.length - 1 ||
-        (sink < 0 || sink > this.adjMatrix.length - 1) || weight <= 0) {
-      System.err.println("Invalid edge: " + source + sink + weight);
+        sink < 0 || sink > this.adjMatrix.length - 1 ||
+        weight <= 0) {
+      System.err.printf("Invalid edge: %d %d %d\n", source, sink, weight);
       return;
     }
     this.countEdges++;
     this.adjMatrix[source][sink] = weight;
   }
 
+  public void addEdgeUnoriented(int source, int sink, int weight) {
+    if (source < 0 || source > this.adjMatrix.length - 1 ||
+        sink < 0 || sink > this.adjMatrix.length - 1 ||
+        weight <= 0) {
+      System.err.printf("Invalid edge: %d %d %d\n", source, sink, weight);
+      return;
+    }
+    this.countEdges += 2;
+    this.adjMatrix[source][sink] = weight;
+    this.adjMatrix[sink][source] = weight;
+  }
+
   public int degree(int node) {
+    if (node < 0 || node > this.adjMatrix.length) {
+      System.out.println("Invalid node: " + node);
+      return -1;
+    }
     int degree = 0;
     for (int j = 0; j < this.adjMatrix[node].length; ++j) {
-      if (adjMatrix[node][j] != 0) {
-        ++degree;
-      }
+      if (this.adjMatrix[node][j] != 0)
+        degree++;
     }
     return degree;
   }
@@ -34,56 +52,112 @@ public class Graph {
     int highest = 0;
     for (int i = 0; i < this.adjMatrix.length; ++i) {
       int degreeI = degree(i);
-      if (degreeI > highest) {
+      if (degreeI > highest)
         highest = degreeI;
-      }
     }
     return highest;
   }
 
   public int lowestDegree() {
-    int lowest = 0;
+    int lowest = this.countNodes;
     for (int i = 0; i < this.adjMatrix.length; ++i) {
-      int degreeI = degree(i);
-      if (degreeI > lowest) {
-        lowest = degreeI;
-      }
+      int degreeNodeI = this.degree(i);
+      if (lowest > degreeNodeI)
+        lowest = degreeNodeI;
     }
     return lowest;
   }
 
   public Graph complement() {
-    Graph aux = new Graph(this.adjMatrix.length);
+    Graph g2 = new Graph(this.countNodes);
     for (int i = 0; i < this.adjMatrix.length; ++i) {
       for (int j = 0; j < this.adjMatrix[i].length; ++j) {
-        if (this.adjMatrix[i][j] == 0 && i != j) {
-          aux.addEdge(i, j, 1);
+        if (i != j && this.adjMatrix[i][j] == 0) {
+          g2.addEdge(i, j, 1);
         }
       }
     }
-    return aux;
+    return g2;
   }
 
-  public boolean subgraph(Graph g2) {
-
-    if (g2.countEdges > this.countEdges || g2.countEdges > this.countNodes) {
-      return false;
-    }
-    for (int i = 0; i < g2.adjMatrix.length; ++i) {
-      for (int j = 0; j < g2.adjMatrix.length; ++j) {
-        if (g2.adjMatrix[i][j] != 0 && this.adjMatrix[i][j] == 0) {
-          return false;
+  public ArrayList<Integer> bfs(int s) {
+    int[] desc = new int[this.countNodes];
+    ArrayList<Integer> Q = new ArrayList<>();
+    Q.add(s);
+    ArrayList<Integer> R = new ArrayList<>();
+    R.add(s);
+    desc[s] = 1;
+    while (Q.size() > 0) {
+      int u = Q.remove(0);
+      for (int v = 0; v < this.adjMatrix[u].length; ++v) {
+        if (this.adjMatrix[u][v] != 0) {
+          if (desc[v] == 0) {
+            Q.add(v);
+            R.add(v);
+            desc[v] = 1;
+          }
         }
       }
     }
-    return true;
+    return R;
+  }
 
+  public ArrayList<Integer> buscaProfundidade(int s) {
+    int[] desc = new int[this.countNodes];
+    ArrayList<Integer> S = new ArrayList<>();
+    ArrayList<Integer> R = new ArrayList<>();
+
+    S.add(s);
+    R.add(s);
+    desc[s] = 1;
+
+    while (S.size() > 0) {
+      int u = S.get(S.size() - 1);
+      boolean in = false;
+      for (int v = 0; v < this.adjMatrix[u].length; ++v) {
+        if (this.adjMatrix[u][v] != 0) {
+          if (desc[v] == 0) {
+            S.add(v);
+            R.add(v);
+            desc[v] = 1;
+            in = true;
+            break;
+          }
+        }
+      }
+
+      if (in == false) {
+        S.remove(S.size() - 1);
+      }
+    }
+    return R;
+  }
+
+  public boolean connected() {
+    if (this.adjMatrix.length == this.bfs(0).size()) {
+      return true;
+    }
+    return false;
+  }
+
+  public float density() {
+    return (float) this.countEdges / (this.countNodes * (this.countNodes - 1));
+  }
+
+  public boolean oriented() {
+    for (int i = 0; i < this.adjMatrix.length; ++i) {
+      for (int j = 0; j < this.adjMatrix[i].length; ++j) {
+        if (this.adjMatrix[i][j] != this.adjMatrix[j][i])
+          return true;
+      }
+    }
+    return false;
   }
 
   public String toString() {
     String str = "";
     for (int i = 0; i < this.adjMatrix.length; ++i) {
-      for (int j = 0; j < this.adjMatrix.length; ++j) {
+      for (int j = 0; j < this.adjMatrix[i].length; ++j) {
         str += this.adjMatrix[i][j] + "\t";
       }
       str += "\n";
